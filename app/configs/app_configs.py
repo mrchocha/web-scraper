1993aa5b44b12beec1b53b1056c6d2c8a7c91d8f
Rahul Chocha <rahulahir4530@gmail.com>
2024-07-24 12:54:55 +0530

    chore: heard code env var

--- app/configs/app_configs.py
+++ app/configs/app_configs.py
@@ -7,35 +7,19 @@ from functools import cached_property
 
 
 class __AppConfigs:
+    ___GLOBAL_ACCESS_TOKEN = "bWc4eDJ6am1kamwzeTc2ZjdvZWt5bDRuMHVpNmp2Yjlicnc0czBpZm9pOXlzemxrc3pldm43Mmt2Ym12bnFyZnpxeThkdm82d3E5bWZkZWNzMG85YjMzZ3k4enRxYW16eWQyNzk3bmlhN3YwMTcxNnBzM29tOXM4bWdzbjd5cjY="
+
     @cached_property
     def get_notification_type(self) -> NotificationTypeEnum:
-        try:
-            env_notification_type = os.environ.get("NOTIFICATION_TYPE")
-            if env_notification_type is None:
-                return NotificationTypeEnum.LOCAL
-
-            return NotificationTypeEnum[env_notification_type.upper()]
-        except:
-            return NotificationTypeEnum.LOCAL
+        return NotificationTypeEnum.LOCAL
 
     @cached_property
     def get_global_access_token(self) -> str:
-        env_global_access_token = os.environ.get("GLOBAL_ACCESS_TOKEN")
-        if env_global_access_token is None:
-            raise NotFoundError(message=f"GLOBAL_ACCESS_TOKEN not defined/found")
-
-        return env_global_access_token
+        return self.___GLOBAL_ACCESS_TOKEN
 
     @cached_property
     def get_db_type(self) -> DBTypeEnum:
-        try:
-            env_db_type = os.environ.get("DB_TYPE")
-            if env_db_type is None:
-                raise DBTypeEnum.LOCAL
-
-            return DBTypeEnum[env_db_type.upper()]
-        except:
-            return DBTypeEnum.LOCAL
+        return DBTypeEnum.LOCAL
 
 
 AppConfigsInstance = __AppConfigs()
